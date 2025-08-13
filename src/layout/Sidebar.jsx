78bf221d247783cd24b962bfd6753c8fbf84c9c4
Abdullah Al Ashrafee <dev.ashrafee@gmail.com>
2025-08-13 10:47:00 +0600

    Sidebar finished

---
+++ src/layout/Sidebar.jsx
@@ -0,0 +1,45 @@
+import { FaUserFriends } from "react-icons/fa";
+import { MdInventory } from "react-icons/md";
+import { MdOutlineInventory2 } from "react-icons/md";
+import { MdSpaceDashboard } from "react-icons/md";
+import React from "react";
+
+const Sidebar = ({ children }) => {
+  return (
+    <div className="flex flex-row gap-10">
+      <div className="h-[100dvh] pl-7 py-7 border-r border-r-gray-200 flex flex-col justify-start">
+        <div className="flex-1 flex flex-col pr-7">
+          <p className="font-poppins text-2xl font-bold text-gray-800">
+            Rentiva - Rental
+          </p>
+          <p className="font-poppins text-[14px] text-gray-600">
+            Business Management
+          </p>
+        </div>
+        <div className="flex-3 flex flex-col gap-0.5 items-center ">
+          <div className="flex flex-row gap-3 items-center pl-10 hover:bg-gray-100 rounded-l-2xl w-full py-3.5 cursor-pointer">
+            <MdSpaceDashboard size={30} className="" />
+            <p className="font-poppins text-[18px] text-gray-800">Dashboard</p>
+          </div>
+          <div className="flex flex-row gap-3 items-center pl-10 hover:bg-gray-100 rounded-l-2xl w-full py-3.5 cursor-pointer">
+            <MdInventory size={28} className="" />
+            <p className="font-poppins text-[18px] text-gray-800">Stock</p>
+          </div>
+          <div className="flex flex-row gap-3 items-center pl-10 hover:bg-gray-100 rounded-l-2xl w-full py-3.5 cursor-pointer">
+            <FaUserFriends size={28} className="" />
+            <p className="font-poppins text-[18px] text-gray-800">Customer</p>
+          </div>
+        </div>
+        <div className="flex-1">
+          <div className="flex flex-row gap-3 items-center pl-10 hover:bg-gray-100 rounded-l-2xl w-full py-3.5 cursor-pointer">
+            <FaUserFriends size={28} className="" />
+            <p className="font-poppins text-[18px] text-gray-800">Log Out</p>
+          </div>
+        </div>
+      </div>
+      <div>{children}</div>
+    </div>
+  );
+};
+
+export default Sidebar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import Sidebar from "../layout/Sidebar";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <Sidebar>
+        <div className="mt-7 flex flex-col">
+          <div className="flex flex-col">
+            <p className="text-4xl font-bold font-poppins leading-12">
+              Dashboard
+            </p>
+            <p className="font-poppins text-gray-500">
+              Welcome to your closet rental business overview
+            </p>
+          </div>
+        </div>
+      </Sidebar>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
